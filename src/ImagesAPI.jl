c7f9a413fc83abef3da23b8545d327a04e634e17
JohnnyChen <johnnychen94@hotmail.com>
2019-05-07 23:15:18 +0800

    add AbstractImageAlgorithm and AbstractImageFilter

--- src/ImagesAPI.jl
+++ src/ImagesAPI.jl
@@ -1,5 +1,17 @@
 module ImagesAPI
 
-greet() = print("Hello World!")
+"""
+    AbstractImageAlgorithm
+
+The root of image algorithms type system
+"""
+abstract type AbstractImageAlgorithm end
+
+"""
+    AbstractImageFilter <: AbstractImageAlgorithm
+
+Filters are image algorithms whose input and output are both images
+"""
+abstract type AbstractImageFilter <: AbstractImageAlgorithm end
 
 end # module
