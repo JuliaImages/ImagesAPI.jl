2f41fa48990b6c349230e9c95c584e2bb0dc1da4
Tokazama <zchristensen7@gmail.com>
2019-08-03 12:16:33 -0400

    Provide trait based opt in for Images API.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
 using ImagesAPI
 using Test
 
+import ImagesAPI: default_names
+
 @testset "ImagesAPI.jl" begin
     # Write your own tests here.
+    img = reshape(1:24, 2,3,4)
+    @test @inferred(namedaxes(img)) == NamedTuple{default_names(img)}(axes(img))
+
+    @test @inferred(HasDimNames(img)) == HasDimNames{false}()
+
+    @test @inferred(HasProperties(img)) == HasProperties{false}()
 end
+
